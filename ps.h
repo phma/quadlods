7687f775eeab4dd8ba597b8f4a8663914553d893
Pierre Abbat <phma@bezitopo.org>
2016-08-10 18:19:05 +0000

    add GPL and LGPL licenses

--- ps.h
+++ ps.h
@@ -3,6 +3,24 @@
 /* ps.h - PostScript output                           */
 /*                                                    */
 /******************************************************/
+/* Copyright 2014 Pierre Abbat.
+ * This file is part of the Quadlods program.
+ * 
+ * The Quadlods program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Quadlods is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and Lesser General Public License along with Quadlods. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 extern FILE *psfile;
 extern int orientation;
--- quadlods.cpp
+++ quadlods.cpp
@@ -12,6 +12,24 @@
  * is at least some specified limit. The exclusive-oring is done in such a way
  * that the result will not exceed the denominator.
  */
+/* Copyright 2014,2016 Pierre Abbat.
+ * This file is part of the Quadlods library.
+ * 
+ * The Quadlods library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Quadlods is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and Lesser General Public License along with Quadlods. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
 #include <iostream>
 #include "quadlods.h"
