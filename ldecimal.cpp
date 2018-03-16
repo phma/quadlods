be4f2c9fc73d4d21db07a760ec96ba631354a00a
Pierre Abbat <phma@bezitopo.org>
2018-03-16 23:27:04 +0000

    Fix license notice in ldecimal

--- ldecimal.cpp
+++ ldecimal.cpp
@@ -3,21 +3,23 @@
 /* ldecimal.cpp - lossless decimal representation     */
 /*                                                    */
 /******************************************************/
-/* Copyright 2015,2017 Pierre Abbat.
- * This file is part of Bezitopo.
+/* Copyright 2018 Pierre Abbat.
+ * This file is part of the Quadlods program.
  * 
- * Bezitopo is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * The Quadlods program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * Bezitopo is distributed in the hope that it will be useful,
+ * Quadlods is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU General Public License and Lesser General Public License
+ * for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ * and Lesser General Public License along with Quadlods. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #include <cstdio>
