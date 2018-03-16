a4e3db88a71a2bf4981383a9ee3bc7ca3f24f867
Pierre Abbat <phma@bezitopo.org>
2018-03-16 23:30:55 +0000

    Plan how to test how well they fill space

---
+++ filltest.h
@@ -0,0 +1,28 @@
+/******************************************************/
+/*                                                    */
+/* filltest.h - test how well numbers fill space      */
+/*                                                    */
+/******************************************************/
+/* Copyright 2018 Pierre Abbat.
+ * This file is part of the Quadlods program.
+ * 
+ * The Quadlods program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Quadlods is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and Lesser General Public License along with Quadlods. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/* For an n-dimensional sequence, pick n random points, and note how close
+ * the quasirandom points get to the random points. Graph the determinant
+ * and the average distance.
+ */
