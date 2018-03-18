b295309a9c615bb7ac78154346b69c715dea10f2
Pierre Abbat <phma@bezitopo.org>
2018-03-18 21:45:53 +0000

    Add matrix and random
    They will be used in filltest.

---
+++ random.h
@@ -0,0 +1,45 @@
+/******************************************************/
+/*                                                    */
+/* random.h - random numbers                          */
+/*                                                    */
+/******************************************************/
+/* Copyright 2018 Pierre Abbat.
+ * This file is part of the Quadlods program.
+ * 
+ * The Quadlods program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Quadlods is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License and Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and Lesser General Public License along with Quadlods. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include "config.h"
+
+class randm
+{
+public:
+  randm();
+  unsigned int uirandom();
+  unsigned short usrandom();
+  unsigned char ucrandom();
+  double expirandom();
+  double expsrandom();
+  double expcrandom();
+  ~randm();
+private:
+#if defined(_WIN32)
+  unsigned int usbuf,ucbuf,usnum,ucnum;
+#else
+  FILE *randfil;
+#endif
+};
+
+extern randm rng;
